6a0e88af346c00eb003cc0763c94d28c322b7992
Daniel Pfeiffer <dpfeiffer@gowithfloat.com>
2017-08-03 13:33:52 -0500

    Disables fastclick on select2 objects

--- js/adapt-fastclick.js
+++ js/adapt-fastclick.js
@@ -1,6 +1,10 @@
 define(function() {
   require(["libraries/fastclick.min.js"], function(FastClick) {
     $(function() {
+      var _needsClick = FastClick.prototype.needsClick;
+      FastClick.prototype.needsClick = function(target) {
+        return _needsClick(target) || /select2/.test(target.className);
+      };
       FastClick.attach(document.body);
     });
   });
